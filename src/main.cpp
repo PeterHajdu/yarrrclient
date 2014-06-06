89459137475ff70419bcc8312eb1885dc36f154c
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-06 12:19:06 +0200

    move read to lower layer

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@
 #include <yarrr/ship.hpp>
 #include <yarrr/network.hpp>
 
-//todo: remove when read is removed
-#include <unistd.h>
-
 namespace
 {
   void new_connection( yarrr::Socket& )
@@ -14,13 +11,9 @@ namespace
     std::cout << "new connection established" << std::endl;
   }
 
-  const int max_message_size( 1000 );
-  std::array<char,max_message_size> messageBuffer;
-  void data_available_on( yarrr::Socket& socket )
+  void data_available_on( yarrr::Socket& socket, char* message, size_t length )
   {
-    //todo: move reading and data buffering to lower layer
-    const size_t length( ::read( socket.fd, &messageBuffer[0], max_message_size ) );
-    std::cout << "data arrived: " << std::string( &messageBuffer[0], &messageBuffer[length] ) << std::endl;
+    std::cout << "data arrived: " << std::string( message, length ) << std::endl;
   }
 }
 
@@ -30,8 +23,10 @@ int main( int argc, char ** argv )
       new_connection,
       data_available_on
       );
-  pool.connect( "localhost", 2000 );
-  pool.start();
+  if ( pool.connect( "localhost", 2000 ) )
+  {
+    pool.start();
+  }
   return 0;
 }
 
