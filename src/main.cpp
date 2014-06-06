e11bc04eb823c310e8491a9fdd492d244481dd35
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-06 11:46:32 +0200

    client connects to server

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,37 @@
+#include <iostream>
+#include <array>
+
+#include <yarrr/ship.hpp>
+#include <yarrr/network.hpp>
+
+//todo: remove when read is removed
+#include <unistd.h>
+
+namespace
+{
+  void new_connection( yarrr::Socket& )
+  {
+    std::cout << "new connection established" << std::endl;
+  }
+
+  const int max_message_size( 1000 );
+  std::array<char,max_message_size> messageBuffer;
+  void data_available_on( yarrr::Socket& socket )
+  {
+    //todo: move reading and data buffering to lower layer
+    const size_t length( ::read( socket.fd, &messageBuffer[0], max_message_size ) );
+    std::cout << "data arrived: " << std::string( &messageBuffer[0], &messageBuffer[length] ) << std::endl;
+  }
+}
+
 int main( int argc, char ** argv )
 {
+  yarrr::SocketPool pool(
+      new_connection,
+      data_available_on
+      );
+  pool.connect( "localhost", 2000 );
+  pool.start();
   return 0;
 }
 
