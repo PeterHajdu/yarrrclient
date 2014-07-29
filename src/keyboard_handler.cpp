98a62b05245a74d44b38791de1b4dd4f918b6eef
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-07-29 08:58:37 +0200

    add cli

--- src/keyboard_handler.cpp
+++ src/keyboard_handler.cpp
@@ -1,9 +1,12 @@
 #include "keyboard_handler.hpp"
+#include <thectci/service_registry.hpp>
 #include <SDL2/SDL.h>
 
 KeyboardHandler::KeyboardHandler( bool& running )
   : m_running( running )
+  , m_cli( 0, 100, the::ctci::service< yarrr::GraphicalEngine >() )
 {
+  SDL_StartTextInput();
 }
 
 void
@@ -14,8 +17,35 @@ KeyboardHandler::send_command( yarrr::Command::Type cmd, the::time::Time& timest
 }
 
 void
+KeyboardHandler::handle_text_input()
+{
+  //todo: split this up somehow
+  SDL_Event event;
+  while( SDL_PollEvent( &event ) )
+  {
+    switch( event.type )
+    {
+      case SDL_TEXTINPUT:
+        m_cli.append( event.text.text );
+        break;
+      case SDL_KEYDOWN:
+        if ( event.key.keysym.sym == SDLK_RETURN )
+        {
+          m_cli.finalize();
+        }
+        else if ( event.key.keysym.sym == SDLK_ESCAPE )
+        {
+          m_running = false;
+        }
+        break;
+    }
+  }
+}
+
+void
 KeyboardHandler::check_keyboard( the::time::Time& now )
 {
+  handle_text_input();
   SDL_PumpEvents();
   const unsigned char *keystates = SDL_GetKeyboardState( nullptr );
   if (keystates[SDL_SCANCODE_RIGHT])
@@ -31,11 +61,6 @@ KeyboardHandler::check_keyboard( the::time::Time& now )
   if (keystates[SDL_SCANCODE_UP])
   {
     send_command( yarrr::Command::thruster, now );
-  }
-
-  if (keystates[SDL_SCANCODE_Q])
-  {
-    m_running = false;
   }
 }
 
--- src/keyboard_handler.hpp
+++ src/keyboard_handler.hpp
@@ -1,14 +1,18 @@
 #pragma once
 #include <yarrr/command.hpp>
+#include <yarrr/cli.hpp>
 #include <thectci/multiplexer.hpp>
 
 class KeyboardHandler : public the::ctci::Multiplexer
 {
   public:
     KeyboardHandler( bool& running );
-    void send_command( yarrr::Command::Type cmd, the::time::Time& timestamp );
     void check_keyboard( the::time::Time& now );
   private:
+    void send_command( yarrr::Command::Type cmd, the::time::Time& timestamp );
+    void handle_text_input();
+
     bool& m_running;
+    yarrr::Cli m_cli;
 };
 
