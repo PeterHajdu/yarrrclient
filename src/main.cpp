bc060ec4b27c4f061552b3d48852542d78fe3264
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-07-30 15:47:51 +0200

    send log messages to the graphical terminal

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <yarrr/graphical_engine.hpp>
 #include <yarrr/basic_behaviors.hpp>
+#include <yarrr/stream_to_chat.hpp>
 #include <thenet/address.hpp>
 #include <thetime/frequency_stabilizer.hpp>
 #include <thetime/clock.hpp>
@@ -17,7 +18,9 @@
 
 int main( int argc, char ** argv )
 {
+  yarrr::StreamToChat stream_to_chat( "system" );
   the::log::Logger::add_channel( std::cout );
+  the::log::Logger::add_channel( stream_to_chat.stream() );
   the::conf::set_value( "login_name", getenv( "LOGNAME" ) );
   the::time::Clock clock;
 
@@ -35,6 +38,7 @@ int main( int argc, char ** argv )
   bool running( true );
   KeyboardHandler keyboard_handler( running );
   keyboard_handler.register_dispatcher( the::ctci::service<LocalEventDispatcher>().dispatcher );
+  stream_to_chat.register_dispatcher( the::ctci::service<LocalEventDispatcher>().dispatcher );
 
   while ( running )
   {
@@ -42,6 +46,8 @@ int main( int argc, char ** argv )
 
     keyboard_handler.check_keyboard( now );
     network_service.process_incoming_messages();
+
+    stream_to_chat.flush();
 
     world.broadcast( yarrr::TimerUpdate( now ) );
     world.in_focus();
