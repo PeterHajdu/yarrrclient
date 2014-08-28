b57dd92f6556f36447acf5bb34aab135824aaedf
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-08-28 14:51:25 +0200

    fix resource lookup test

--- test/test_resources.cpp
+++ test/test_resources.cpp
@@ -1,18 +1,20 @@
-int access(const char *path, int mode)
-{
-  return 0;
-}
-
 #include "../src/resources.hpp"
 #include <igloo/igloo_alt.h>
+#include <fstream>
 
 using namespace igloo;
 
 //context dependent tests, I wasn't able to mock access()
 Describe( a_resource_finder )
 {
+  void touch_file( const std::string& path )
+  {
+    std::ofstream touched_file( path );
+  }
+
   void SetUp()
   {
+    touch_file( second_folder + file_only_in_second_folder );
     resource_finder.reset( new yarrr::ResourceFinder( path_list ) );
   }
 
@@ -24,14 +26,13 @@ Describe( a_resource_finder )
 
   It( returns_cstr_convertable_path )
   {
-    std::cout << "test: " << resource_finder->find( "cp" ).c_str() << std::endl;
     AssertThat( resource_finder->find( "cp" ).c_str(), Equals( "/bin/cp" ) );
   }
 
   It( returns_path_of_the_first_match )
   {
-    const std::string full_path( resource_finder->find( "diff" ) );
-    AssertThat( full_path, Equals( "/usr/bin/diff" ) );
+    const std::string full_path( resource_finder->find( file_only_in_second_folder ) );
+    AssertThat( full_path, Equals( second_folder + file_only_in_second_folder ) );
   }
 
   It( returns_empty_string_if_the_file_was_not_found )
@@ -42,6 +43,8 @@ Describe( a_resource_finder )
 
 
   std::unique_ptr< yarrr::ResourceFinder > resource_finder;
-  const yarrr::ResourceFinder::PathList path_list{{ "/bin/", "/usr/bin/", "/home/" }};
+  const std::string file_only_in_second_folder{ "appletree" };
+  const std::string second_folder{ "/tmp/" };
+  const yarrr::ResourceFinder::PathList path_list{{ "/bin/", second_folder, "/home/" }};
 };
 
