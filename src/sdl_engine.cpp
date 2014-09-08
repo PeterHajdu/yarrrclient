602699cf53274462de5ffd33ff44862903859dfe
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-09-08 13:22:19 +0200

    decrease radar zoom

--- src/sdl_engine.cpp
+++ src/sdl_engine.cpp
@@ -301,7 +301,7 @@ void
 SdlEngine::show_on_radar( const yarrr::Coordinate& coordinate )
 {
   const yarrr::Coordinate diff(
-      ( yarrr::huplons_to_metres( coordinate ) - m_center_in_metres ) * 0.005 +
+      ( yarrr::huplons_to_metres( coordinate ) - m_center_in_metres ) * 0.01 +
       m_center_of_radar );
   draw_point( diff.x, diff.y, 4, yarrr::Colour::White );
 }
