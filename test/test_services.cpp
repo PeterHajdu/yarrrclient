5463f19ef49fde4d1a15ad8f24e81c1c363c531d
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-10-14 14:02:06 +0200

    world instantiated in unit test

--- test/test_services.cpp
+++ test/test_services.cpp
@@ -1,3 +1,4 @@
+#include "../src/local_event_dispatcher.hpp"
 #include <yarrr/graphical_engine.hpp>
 #include <yarrr/test_graphical_engine.hpp>
 #include <thectci/service_registry.hpp>
@@ -8,5 +9,7 @@ namespace
 the::ctci::AutoServiceRegister< yarrr::GraphicalEngine, test::GraphicalEngine >
   auto_test_graphical_engine_register;
 
+the::ctci::AutoServiceRegister< LocalEventDispatcher, LocalEventDispatcher >
+  local_event_dispatcher_register;
 }
 
